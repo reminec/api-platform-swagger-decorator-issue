89ab36169c7b4e75e0616e4c2fc91c3fed2595ef
reminec <remi.nectoux@gmail.com>
2017-09-16 08:57:43 +0200

    fresh project + swagger decorator

---
+++ src/AppBundle/Swagger/SwaggerDecorator.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace AppBundle\Swagger;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class SwaggerDecorator implements NormalizerInterface
+{
+    private $decorated;
+
+    public function __construct(NormalizerInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $docs = $this->decorated->normalize($object, $format, $context);
+		
+		// Override title
+		$docs['info']['title'] = 'My Api Foo';
+			
+        return $docs;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+}
+
